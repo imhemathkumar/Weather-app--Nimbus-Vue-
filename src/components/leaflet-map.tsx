178cca38c6d3ad716d702f08e0d968ab8ea5dbf1
Hemanth Kumar Telukuntla <hemanthkumartelukuntla143@gmail.com>
2025-01-08 12:47:05 +0530

    Update leaflet-map.tsx

--- src/components/leaflet-map.tsx
+++ src/components/leaflet-map.tsx
@@ -1,18 +1,17 @@
 'use client'
 
-import type { Map as LeafletMap } from 'leaflet'
+import { Map as LeafletMap, LeafletMouseEvent } from 'leaflet'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import { useEffect } from 'react'
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
-import type { LeafletMouseEvent } from 'leaflet'
 
 // Fix for default marker icon
-delete (L.Icon.Default.prototype as { _getIconUrl?: () => string })._getIconUrl
+delete (L.Icon.Default.prototype as any)._getIconUrl
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon-2x.png',
-  iconUrl: 'https://cdnjscloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png',
-  shadowUrl: 'https://cdnjscloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png',
 })
 
 interface LeafletMapProps {
