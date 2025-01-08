ffcfc12ba592bf2f00b61bdf73c3852a80974d14
Hemanth Kumar Telukuntla <hemanthkumartelukuntla143@gmail.com>
2025-01-08 13:45:07 +0530

    Update leaflet-map.tsx

--- src/components/leaflet-map.tsx
+++ src/components/leaflet-map.tsx
@@ -6,8 +6,8 @@ import { useEffect } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
 // Fix for default marker icon
-delete (L as any).Icon.Default.prototype._getIconUrl;
-(L as any).Icon.Default.mergeOptions({
+delete (L as unknown as { Icon: { Default: { prototype: { _getIconUrl: unknown } } } }).Icon.Default.prototype._getIconUrl;
+(L as unknown as { Icon: { Default: { mergeOptions: (options: Record<string, string>) => void } } }).Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon-2x.png',
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png',
