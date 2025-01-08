d7c7214614c438e42b35545b71f9b19cb230c5fe
Hemanth Kumar Telukuntla <hemanthkumartelukuntla143@gmail.com>
2025-01-08 13:12:45 +0530

    Update leaflet-map.tsx

--- src/components/leaflet-map.tsx
+++ src/components/leaflet-map.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import * as L from 'leaflet'
+import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import { useEffect } from 'react'
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 
 // Fix for default marker icon
-delete L.Icon.Default.prototype._getIconUrl
-L.Icon.Default.mergeOptions({
+delete (L as any).Icon.Default.prototype._getIconUrl;
+(L as any).Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon-2x.png',
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png',
