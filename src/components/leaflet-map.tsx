78a06ff8bdfb0d7378a1ce8d3cb4b82bb05f7953
Hemanth Kumar Telukuntla <hemanthkumartelukuntla143@gmail.com>
2025-01-08 15:30:58 +0530

    Update leaflet-map.tsx

--- src/components/leaflet-map.tsx
+++ src/components/leaflet-map.tsx
@@ -5,11 +5,15 @@ import 'leaflet/dist/leaflet.css';
 import { useEffect } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
-// Create custom icon with L.icon()
+// Fix for default marker icon (use L.icon() to create a new icon)
 const customIcon = L.icon({
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png',
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon-2x.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
 });
 
 interface LeafletMapProps {
@@ -78,3 +82,4 @@ export default function LeafletMap({ center, onLocationSelect, weatherInfo }: Le
     </MapContainer>
   );
 }
+
