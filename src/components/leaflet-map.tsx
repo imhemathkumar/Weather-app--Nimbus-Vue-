8b7781e3cf214302d8a21960f7a2242e83cf7d91
Hemanth Kumar Telukuntla <hemanthkumartelukuntla143@gmail.com>
2025-01-08 12:28:00 +0530

    Update leaflet-map

--- src/components/leaflet-map.tsx
+++ src/components/leaflet-map.tsx
@@ -1,4 +1,4 @@
-declare module 'leaflet';
+/// <reference types="leaflet" />
 
 'use client'
 
@@ -13,7 +13,7 @@ delete (L.Icon.Default.prototype as { _getIconUrl?: () => string })._getIconUrl
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon-2x.png',
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png',
+  shadowUrl: 'https://cdnjscloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png',
 })
 
 interface LeafletMapProps {
